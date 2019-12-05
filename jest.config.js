567aac85f1ccf10ccf916cdd0b2b9c6c2744f88b
E-Liang Tan <eliang@eliangtan.com>
2019-12-05 17:28:22 +0800

    Add CircleCI config and Codecov

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverageFrom: ['**/!(*.d).{js,jsx,ts,tsx}'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  // Only write lcov files in CIs
+  coverageReporters: ['text'].concat(process.env.CI ? 'json' : []),
 };
