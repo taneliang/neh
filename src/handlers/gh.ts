c13bdb09e1a6fddb94e9d9ae05c3eb651d599c95
E-Liang Tan <eliang@eliangtan.com>
2020-04-01 12:02:45 +0800

    Refactor some common code

--- src/handlers/gh.ts
+++ src/handlers/gh.ts
@@ -1,4 +1,4 @@
-import { CommandHandler, FunctionHandler, RedirectHandler } from '../Handler';
+import { CommandHandler, FunctionHandler, RedirectHandler, HandlerFn } from '../Handler';
 import { SearchEngineHandler, makeParamBasedSearchEngine } from '../SearchEngineHandler';
 import { redirect } from '../util';
 
@@ -14,70 +14,45 @@ gh.setDefaultHandler(
   ),
 );
 
+const makeGitHubSearchHandlerFn = (resource: string): HandlerFn => async (
+  tokens,
+): Promise<Response> => {
+  const query = tokens.join('+');
+  const url = new URL(`https://api.github.com/search/${resource}`);
+  url.searchParams.set('q', query);
+  url.searchParams.set('per_page', '1');
+  const searchUrl = url.toString();
+
+  const response = await fetch(searchUrl, {
+    headers: {
+      'User-Agent': 'neh.eltan.net',
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const body = await response.text();
+    return new Response(`GitHub API returned ${response.status} ${response.statusText}. ${body}`, {
+      status: 500,
+    });
+  }
+
+  const { items } = await response.json();
+  if (items.length === 0) {
+    return new Response(`No profiles found for query ${query}.`, { status: 500 });
+  }
+
+  return redirect(items[0].html_url);
+};
+
 gh.addHandler(
   'p',
-  new FunctionHandler('navigates to a GitHub user profile', async (tokens) => {
-    const query = tokens.join('+');
-    const url = new URL('https://api.github.com/search/users');
-    url.searchParams.set('q', query);
-    url.searchParams.set('per_page', '1');
-    const searchUrl = url.toString();
-
-    const response = await fetch(searchUrl, {
-      headers: {
-        'User-Agent': 'neh.eltan.net',
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (!response.ok) {
-      const body = await response.text();
-      return new Response(
-        `GitHub API returned ${response.status} ${response.statusText}. ${body}`,
-        { status: 500 },
-      );
-    }
-
-    const { items } = await response.json();
-    if (items.length === 0) {
-      return new Response(`No profiles found for query ${query}.`, { status: 500 });
-    }
-
-    return redirect(items[0].html_url);
-  }),
+  new FunctionHandler('navigates to a GitHub user profile', makeGitHubSearchHandlerFn('users')),
 );
 
 gh.addHandler(
   'r',
-  new FunctionHandler('navigates to a GitHub repo', async (tokens) => {
-    const query = tokens.join('+');
-    const url = new URL('https://api.github.com/search/repositories');
-    url.searchParams.set('q', query);
-    url.searchParams.set('per_page', '1');
-    const searchUrl = url.toString();
-
-    const response = await fetch(searchUrl, {
-      headers: {
-        'User-Agent': 'neh.eltan.net',
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (!response.ok) {
-      const body = await response.text();
-      return new Response(
-        `GitHub API returned ${response.status} ${response.statusText}. ${body}`,
-        { status: 500 },
-      );
-    }
-
-    const { items } = await response.json();
-    if (items.length === 0) {
-      return new Response(`No repositories found for query ${query}.`, { status: 500 });
-    }
-
-    return redirect(items[0].html_url);
-  }),
+  new FunctionHandler('navigates to a GitHub repo', makeGitHubSearchHandlerFn('repositories')),
 );
 
 export default gh;
