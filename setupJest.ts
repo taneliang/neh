363c0c56b0ac1d41aa81d30a939bc309669490fb
E-Liang Tan <eliang@eliangtan.com>
2020-04-01 12:07:11 +0800

    Upgrade gh p/r handlers to query GitHub API for redirect URL (#145)

--- setupJest.ts
+++ setupJest.ts
@@ -1,2 +1,8 @@
 import makeCloudflareWorkerEnv from 'cloudflare-worker-mock';
+import jestFetchMock, { GlobalWithFetchMock } from 'jest-fetch-mock';
+
 Object.assign(global, makeCloudflareWorkerEnv());
+
+const customGlobal = global as GlobalWithFetchMock;
+customGlobal.fetch = jestFetchMock;
+customGlobal.fetchMock = customGlobal.fetch;
--- src/handlers/gh.ts
+++ src/handlers/gh.ts
@@ -1,9 +1,6 @@
-import { CommandHandler, RedirectHandler } from '../Handler';
-import {
-  SearchEngineHandler,
-  makeParamBasedSearchEngine,
-  makePathBasedSearchEngine,
-} from '../SearchEngineHandler';
+import { CommandHandler, FunctionHandler, RedirectHandler, HandlerFn } from '../Handler';
+import { SearchEngineHandler, makeParamBasedSearchEngine } from '../SearchEngineHandler';
+import { redirect } from '../util';
 
 const gh = new CommandHandler();
 const ghHomeUrl = 'https://github.com/';
@@ -17,14 +14,51 @@ gh.setDefaultHandler(
   ),
 );
 
-const ghPathEngine = makePathBasedSearchEngine(
-  ghHomeUrl,
-  null,
-  [0, 1], // username/repo
+const makeGitHubSearchHandlerFn = (resource: string): HandlerFn => async (
+  tokens,
+): Promise<Response> => {
+  const query = tokens.join('+');
+  const url = new URL(`https://api.github.com/search/${resource}`);
+  url.searchParams.set('q', query);
+  url.searchParams.set('per_page', '1');
+  const searchUrl = url.toString();
+
+  const response = await fetch(searchUrl, {
+    headers: {
+      'User-Agent': 'neh.eltan.net',
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const body = await response.text();
+    return new Response(`GitHub API returned ${response.status} ${response.statusText}. ${body}`, {
+      status: 500,
+    });
+  }
+
+  const { items } = await response.json();
+  if (items.length === 0) {
+    return new Response(`No ${resource} found for query ${query}.`, { status: 500 });
+  }
+
+  return redirect(items[0].html_url);
+};
+
+gh.addHandler(
+  'p',
+  new FunctionHandler(
+    'navigates to a GitHub user/organization profile',
+    makeGitHubSearchHandlerFn('users'),
+  ),
 );
 
-gh.addHandler('p', new SearchEngineHandler('navigates to a GitHub user profile', ghPathEngine));
-
-gh.addHandler('r', new SearchEngineHandler('navigates to a GitHub repo', ghPathEngine));
+gh.addHandler(
+  'r',
+  new FunctionHandler(
+    'navigates to a GitHub repository',
+    makeGitHubSearchHandlerFn('repositories'),
+  ),
+);
 
 export default gh;
